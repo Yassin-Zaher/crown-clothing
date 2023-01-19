60cf332d9a70f9f3e7cca85e721c0d3048d07c31
Yassin Zaher <yassinzaherelfeliri@gmail.com>
2023-01-19 13:08:27 +0100

    refactore to sontexte to observable

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import Home from "./routes/home/home.component";
 import { Route, Routes } from "react-router-dom";
 import Navigation from "./routes/navigation/navigation.component";
 import Authentification from "./routes/authentification/authentification.component";
+import Shop from "./routes/shop/shop.component";
 const App = () => {
   return (
     <Routes>
       <Route path="" element={<Navigation />}>
         <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
         <Route path="auth" element={<Authentification />} />
       </Route>
     </Routes>
