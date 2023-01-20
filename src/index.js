9b15e01764c05ffcec72895f7f36df1bdea94813
Yassin Zaher <yassinzaherelfeliri@gmail.com>
2023-01-20 14:37:56 +0100

    create shop component init config

--- src/index.js
+++ src/index.js
@@ -6,13 +6,16 @@ import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { UserProvider } from "./contexts/user.context";
+import { ProductProvider } from "./contexts/products.context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductProvider>
+          <App />
+        </ProductProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
