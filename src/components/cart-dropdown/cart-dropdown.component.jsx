54bb65170f684654537bc94cdfea2dccd23e7db5
Yassin Zaher <yassinzaherelfeliri@gmail.com>
2023-01-21 11:24:36 +0100

    refactore cartContext to use toggleCart

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,10 +1,18 @@
+import { useContext } from "react";
 import "./cart-dropdown.setyles.scss";
 import Button from "../button/button.component";
+import CartItem from "../cart-item/cart-item.component";
+import { CartContext } from "../../contexts/cart.context";
 
 const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
   return (
     <div className="cart-dropdown-container">
-      <div className="items-items" />
+      <div className="items-items">
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
+      </div>
       <Button>GO TO CKECKOUT</Button>
     </div>
   );
