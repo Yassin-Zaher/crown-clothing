e5c062a6a7b2161d32754d3f7e5d233a44709541
Yassin Zaher <yassinzaherelfeliri@gmail.com>
2023-02-01 19:14:53 +0100

    refactore to useReducre

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useEffect, useState, useReducer } from "react";
 import {
   createUserDocumentFromAuth,
   onAuthStateChangedListener,
@@ -10,8 +10,32 @@ export const UserContext = createContext({
   setCurrentUser: () => null,
 });
 
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+
+const userReducer = (state, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload,
+      };
+
+    default:
+      throw Error(`Unhandled type ${type} in userReducer`);
+  }
+};
+
+const INITAL_STATE = {
+  currentUser: null,
+};
+
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
+  //const [currentUser, setCurrentUser] = useState(null);
+  const [state, dispatch] = useReducer(userReducer, INITAL_STATE);
   const value = { currentUser, setCurrentUser };
 
   useEffect(() => {
@@ -25,3 +49,5 @@ export const UserProvider = ({ children }) => {
   }, []);
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
+
+/*  */
